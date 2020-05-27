da5d9110e3c1dfe5e5f425b7f44e1e21cf50c7f8
James-Stewart110 <jamesstewart506@hotmail.co.uk>
2020-05-27 11:16:49 +0000

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,104 @@
+//Card info
+const cardsArray = [
+    {name:'highkick', img:'assets/images/high-kick.jpg'},
+    {name:'block', img:'assets/images/block.jpg'},
+    {name:'bodyshot', img:'assets/images/bodyshot.jpg'},
+    {name:'boxer', img:'assets/images/boxer.jpg'},
+    {name:'colour-block', img:'assets/images/colour-block.jpg'},
+    {name:'jab', img:'assets/images/jab.jpg'},
+    {name:'knee', img:'assets/images/knee.jpg'},
+    {name:'lowkick', img:'assets/images/lowkick.jpg'},
+    {name:'spin-elbow', img:'assets/images/spin-elbow.jpg'},
+    {name:'superman', img:'assets/images/superman.jpg'},
+    {name:'wai', img:'assets/images/wai.jpg'},
+    {name:'women-highkick', img:'assets/images/women-highkick.jpg'}
+];
+
+const gameGrid = cardsArray
+  .concat(cardsArray)
+  .sort(() => 0.5 - Math.random());
+
+let firstGuess = '';
+let secondGuess = '';
+let count = 0;
+let previousTarget = null;
+let delay = 1200;
+
+const game = document.getElementById('game');
+const grid = document.createElement('section');
+grid.setAttribute('class', 'grid');
+game.appendChild(grid);
+
+gameGrid.forEach(item => {
+  const { name, img } = item;
+
+  const card = document.createElement('div');
+  card.classList.add('card');
+  card.dataset.name = name;
+
+  const front = document.createElement('div');
+  front.classList.add('front');
+
+  const back = document.createElement('div');
+  back.classList.add('back');
+  back.style.backgroundImage = `url(${img})`;
+
+  grid.appendChild(card);
+  card.appendChild(front);
+  card.appendChild(back);
+});
+
+const match = () => {
+  const selected = document.querySelectorAll('.selected');
+  selected.forEach(card => {
+    card.classList.add('match');
+  });
+};
+
+const resetGuesses = () => {
+  firstGuess = '';
+  secondGuess = '';
+  count = 0;
+  previousTarget = null;
+
+  var selected = document.querySelectorAll('.selected');
+  selected.forEach(card => {
+    card.classList.remove('selected');
+  });
+};
+
+grid.addEventListener('click', event => {
+
+  const clicked = event.target;
+
+  if (
+    clicked.nodeName === 'SECTION' ||
+    clicked === previousTarget ||
+    clicked.parentNode.classList.contains('selected') ||
+    clicked.parentNode.classList.contains('match')
+  ) {
+    return;
+  }
+
+  if (count < 2) {
+    count++;
+    if (count === 1) {
+      firstGuess = clicked.parentNode.dataset.name;
+      console.log(firstGuess);
+      clicked.parentNode.classList.add('selected');
+    } else {
+      secondGuess = clicked.parentNode.dataset.name;
+      console.log(secondGuess);
+      clicked.parentNode.classList.add('selected');
+    }
+
+    if (firstGuess && secondGuess) {
+      if (firstGuess === secondGuess) {
+        setTimeout(match, delay);
+      }
+      setTimeout(resetGuesses, delay);
+    }
+    previousTarget = clicked;
+  }
+
+});
